e1611875016bca401fe45ca885bf5185511f9a68
StruffelProductions <31403260+StruffelProductions@users.noreply.github.com>
2020-09-18 21:49:28 +0200

    changed to using gamma function of imaging

--- main.go
+++ main.go
@@ -3,10 +3,8 @@ package main
 import (
 	"fmt"
 	"image"
-	"image/color"
 	"io/ioutil"
 	"log"
-	"math"
 	"path/filepath"
 
 	"github.com/disintegration/imaging"
@@ -25,35 +23,23 @@ func main() {
 	}
 
 	for _, f := range files {
-		fmt.Println(f.Name())
+		fmt.Print(".")
 		var img, _ = imaging.Open("./input/" + f.Name())
-		fmt.Println(getAverageLuminance(img))
 		sum += getAverageLuminance(img)
 		images++
 	}
+	fmt.Print("\n")
 
 	average = sum / images
-	fmt.Println("AVG")
-	fmt.Println(average)
-
-	//var imgCorrected = imaging.AdjustGamma(img, averageLog/currentLog)
+	fmt.Printf("AVG: %v", average)
 
 	var current uint64 = 0
 
 	for _, f := range files {
 		var img, _ = imaging.Open("./input/" + f.Name())
 		current = getAverageLuminance(img)
-		perPixelFunction := func(c color.NRGBA) color.NRGBA {
-			var output color.NRGBA
-			output.R = uint8(math.Pow((float64(c.R)/255), float64(current)/float64(average)) * 255)
-			output.G = uint8(math.Pow((float64(c.G)/255), float64(current)/float64(average)) * 255)
-			output.B = uint8(math.Pow((float64(c.B)/255), float64(current)/float64(average)) * 255)
-			output.A = c.A
-			return output
-		}
-		var imgCorrected = imaging.AdjustFunc(img, perPixelFunction)
-		fmt.Println(f.Name())
-		fmt.Println(getAverageLuminance(imgCorrected))
+		var imgCorrected = imaging.AdjustGamma(img, float64(average)/float64(current))
+		fmt.Print(".")
 		imaging.Save(imgCorrected, "./output/"+filepath.Base(f.Name()))
 	}
 }
@@ -72,12 +58,3 @@ func getAverageLuminance(input image.Image) uint64 {
 	}
 	return sum / pixels
 }
-
-/*
-func getExposureLUTfromAverageLuminance(current uint16, target uint16) []uint8 {
-	var lut = make([]uint8, 256)
-	for i := 0; i < 256; i++ {
-		lut[i] = uint8(math.Pow((float64(i) / 256), float64(current)/float64(target)))
-	}
-	return lut
-}*/
