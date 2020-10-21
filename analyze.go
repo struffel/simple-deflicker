303cb26868ab523223e52d59f1a5caa37c28c9b9
StruffelProductions <31403260+StruffelProductions@users.noreply.github.com>
2020-10-21 20:37:40 +0200

    Changed variable names

--- analyze.go
+++ analyze.go
@@ -19,16 +19,16 @@ func measureIntensity(input image.Image, precision int) uint16 {
 	return uint16(sum / pixels)
 }
 
-func measureContrast(input image.Image, averageBrightness uint16, precision int) float64 {
+func measureContrast(input image.Image, averageIntensity uint16, precision int) float64 {
 	var sum float64
 	var pixels uint64
-	var averageBrightnessNormalized = float64(averageBrightness) / 65536.0
+	var averageIntensityNormalized = float64(averageIntensity) / 65536.0
 	for y := input.Bounds().Min.Y; y < input.Bounds().Max.Y; y += precision {
 		for x := input.Bounds().Min.X; x < input.Bounds().Max.X; x += precision {
 			r, g, b, a := input.At(x, y).RGBA()
 			if a > 0 {
-				brightnessNormalized := float64((0.2126*float32(r) + 0.7152*float32(g) + 0.0722*float32(b))) / 65536.0
-				sum += math.Pow((brightnessNormalized-averageBrightnessNormalized)/averageBrightnessNormalized, 2.0)
+				intensityNormalized := float64((0.2126*float32(r) + 0.7152*float32(g) + 0.0722*float32(b))) / 65536.0
+				sum += math.Pow((intensityNormalized-averageIntensityNormalized)/averageIntensityNormalized, 2.0)
 				pixels++
 			}
 		}
@@ -52,16 +52,16 @@ func measureKelvin(input image.Image, precision int) uint16 {
 	return uint16(sum / pixels)
 }
 
-func calculateIntensityDifference(input image.Image, targetBrightness uint16, precision int) float64 {
+func calculateIntensityDifference(input image.Image, targetIntensity uint16, precision int) float64 {
 	var sum int64
 	var pixels uint64
 	for y := input.Bounds().Min.Y; y < input.Bounds().Max.Y; y += precision {
 		for x := input.Bounds().Min.X; x < input.Bounds().Max.X; x += precision {
 			r, g, b, a := input.At(x, y).RGBA()
 			if a > 0 {
-				brightness := uint64(0.2126*float32(r) + 0.7152*float32(g) + 0.0722*float32(b))
-				if brightness != 0 && brightness != 65536 {
-					sum += int64(targetBrightness) - int64(brightness)
+				intensity := uint64(0.2126*float32(r) + 0.7152*float32(g) + 0.0722*float32(b))
+				if intensity != 0 && intensity != 65536 {
+					sum += int64(targetIntensity) - int64(intensity)
 					pixels++
 				}
 			}
@@ -70,16 +70,16 @@ func calculateIntensityDifference(input image.Image, targetBrightness uint16, pr
 	return float64(sum) / float64(pixels)
 }
 
-func calculateGammaDifference(input image.Image, targetBrightness uint16, precision int) float64 {
+func calculateGammaDifference(input image.Image, targetIntensity uint16, precision int) float64 {
 	var sum float64
 	var pixels uint64
 	for y := input.Bounds().Min.Y; y < input.Bounds().Max.Y; y += precision {
 		for x := input.Bounds().Min.X; x < input.Bounds().Max.X; x += precision {
 			r, g, b, a := input.At(x, y).RGBA()
 			if a > 0 {
-				brightness := uint64(0.2126*float32(r) + 0.7152*float32(g) + 0.0722*float32(b))
-				if brightness != 0 && brightness != 65536 {
-					sum += math.Log(clamp(float64(brightness), 1.0, 65535.0)/65536.0) / math.Log(clamp(float64(targetBrightness), 1.0, 65535.0)/65536.0)
+				intensity := uint64(0.2126*float32(r) + 0.7152*float32(g) + 0.0722*float32(b))
+				if intensity != 0 && intensity != 65536 {
+					sum += math.Log(clamp(float64(intensity), 1.0, 65535.0)/65536.0) / math.Log(clamp(float64(targetIntensity), 1.0, 65535.0)/65536.0)
 					pixels++
 				}
 			}
