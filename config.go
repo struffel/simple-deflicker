90b030baefb98d31fd45185ba4d95c0211c20a76
StruffelProductions <31403260+StruffelProductions@users.noreply.github.com>
2021-01-09 13:06:50 +0100

    Add reaction for closing the destination selection dialog
    Software will now close if the "select destination" dialog is closed.

--- config.go
+++ config.go
@@ -38,7 +38,7 @@ func collectConfigInformation() configuration {
 	//Test for missing directory inputs
 	if config.sourceDirectory == "" {
 		dialog.Message("%s", "No source directory has been specified.\nPlease specify a directory now.").Title("Specify source directory").Info()
-		srcDir, err := dialog.Directory().Title("Specify source directory.").Browse()
+		sourceDirectory, err := dialog.Directory().Title("Specify source directory.").Browse()
 		if err != nil {
 			if errors.Is(err, dialog.ErrCancelled) {
 				dialog.Message("%s", "Source directory selection was canceled. The program will now close.").Title("No Source Selected").Info()
@@ -49,13 +49,23 @@ func collectConfigInformation() configuration {
 			}
 		}
 
-		config.sourceDirectory = srcDir
+		config.sourceDirectory = sourceDirectory
 	}
 	if config.destinationDirectory == "" {
 		if dialog.Message("No destination directory has been specified.\nWould you like to use '%s' as a destination directoy?", filepath.Join(config.sourceDirectory, "deflickered")).Title("Specify destination directory").YesNo() {
 			config.destinationDirectory = filepath.Join(config.sourceDirectory, "deflickered")
 		} else {
-			config.destinationDirectory, _ = dialog.Directory().Title("Specify destination directory.").Browse()
+			destinationDirectory, err := dialog.Directory().Title("Specify destination directory.").Browse()
+			if err != nil {
+				if errors.Is(err, dialog.ErrCancelled) {
+					dialog.Message("%s", "Destination directory selection was canceled. The program will now close.").Title("No Destination Selected").Info()
+					os.Exit(1)
+				} else {
+					dialog.Message("%s: %v", "Unexpected error", err).Title("Unexpected Error").Error()
+					log.Fatalln(err)
+				}
+			}
+			config.destinationDirectory = destinationDirectory
 		}
 	}
 	return config
